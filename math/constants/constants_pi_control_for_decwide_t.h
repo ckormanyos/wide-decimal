bbed9dbcbd6eab5b002c196cc5763a4db311505d
Christopher Kormanyos <ckormanyos@yahoo.com>
2021-01-06 14:40:40 +0100

    Fix name and type and date inconsistencies

--- math/constants/constants_pi_control_for_decwide_t.h
+++ math/constants/constants_pi_control_for_decwide_t.h
@@ -1,3 +1,10 @@
+///////////////////////////////////////////////////////////////////////////////
+//  Copyright Christopher Kormanyos 2020 - 2021.
+//  Distributed under the Boost Software License,
+//  Version 1.0. (See accompanying file LICENSE_1_0.txt
+//  or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
 #ifndef CONSTANTS_PI_CONTROL_FOR_DECWIDE_T_2020_11_21_H_
   #define CONSTANTS_PI_CONTROL_FOR_DECWIDE_T_2020_11_21_H_
 
