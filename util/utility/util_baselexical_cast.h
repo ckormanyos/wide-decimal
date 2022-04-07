b5a89ca61fd619eed14d28c52c3d0d20e693d66a
Christopher Kormanyos <ckormanyos@yahoo.com>
2022-04-07 20:26:35 +0200

    Try again to fix one coverity issue

--- util/utility/util_baselexical_cast.h
+++ util/utility/util_baselexical_cast.h
@@ -23,7 +23,7 @@
     using output_value_type = typename std::iterator_traits<OutputIterator>::value_type;
 
   public:
-    static auto extract(output_value_type) -> char = delete;
+    static auto extract(output_value_type) noexcept -> output_value_type = delete;
   };
 
   template<typename OutputIterator,
@@ -34,20 +34,24 @@
     using output_value_type = typename std::iterator_traits<OutputIterator>::value_type;
 
   public:
-    static auto extract(output_value_type c) -> output_value_type
+    static auto extract(output_value_type c) noexcept -> output_value_type
     {
       if(c <= static_cast<output_value_type>(9)) // NOLINT(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
       {
-        c = static_cast<output_value_type>(c + static_cast<output_value_type>('0'));
+        c =
+          static_cast<output_value_type>
+          (
+            c + static_cast<output_value_type>('0')
+          );
       }
       else if((c >= static_cast<output_value_type>(0xA)) && (c <= static_cast<output_value_type>(0xF))) // NOLINT(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
       {
         c =
-        static_cast<output_value_type>
-        (
-            static_cast<output_value_type>(UpperCase ? static_cast<output_value_type>('A') : static_cast<output_value_type>('a'))
-          + static_cast<output_value_type>(c - static_cast<output_value_type>(0xA)) // NOLINT(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
-        );
+          static_cast<output_value_type>
+          (
+              static_cast<output_value_type>(UpperCase ? static_cast<output_value_type>('A') : static_cast<output_value_type>('a'))
+            + static_cast<output_value_type>(c - static_cast<output_value_type>(0xA)) // NOLINT(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+          );
       }
 
       return c;
@@ -62,11 +66,15 @@
     using output_value_type = typename std::iterator_traits<OutputIterator>::value_type;
 
   public:
-    static auto extract(output_value_type c) -> output_value_type
+    static auto extract(output_value_type c) noexcept -> output_value_type
     {
       if(c <= static_cast<output_value_type>(9)) // NOLINT(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
       {
-        c = static_cast<output_value_type>(c + static_cast<output_value_type>('0'));
+        c =
+          static_cast<output_value_type>
+          (
+            c + static_cast<output_value_type>('0')
+          );
       }
 
       return c;
@@ -98,20 +106,6 @@
       auto c = static_cast<output_value_type>(x % static_cast<unsigned_integer_type>(BaseRepresentation));
 
       x = static_cast<unsigned_integer_type>(x / static_cast<unsigned_integer_type>(BaseRepresentation));
-
-      if(c <= static_cast<output_value_type>(9)) // NOLINT(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
-      {
-        c = static_cast<output_value_type>(c + static_cast<output_value_type>('0'));
-      }
-      else if((c >= static_cast<output_value_type>(0xA)) && (c <= static_cast<output_value_type>(0xF))) // NOLINT(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
-      {
-        c =
-        static_cast<output_value_type>
-        (
-            static_cast<output_value_type>(UpperCase ? static_cast<output_value_type>('A') : static_cast<output_value_type>('a'))
-          + static_cast<output_value_type>(c - static_cast<output_value_type>(0xA)) // NOLINT(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
-        );
-      }
 
       *out_first = baselexical_cast_helper<OutputIterator, UpperCase, BaseRepresentation>::extract(c);
     }
