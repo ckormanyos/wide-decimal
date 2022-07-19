73e4875286af83fc9eb9c5232908e3c27d9a56d7
Christopher Kormanyos <ckormanyos@yahoo.com>
2022-07-19 15:33:27 +0200

    Fine tune the STM32F429 startup code

--- test/target/micros/stm32f429/make/single/crt.cpp
+++ test/target/micros/stm32f429/make/single/crt.cpp
@@ -1,4 +1,4 @@
-#include <algorithm>
+﻿#include <algorithm>
 #include <array>
 #include <cstddef>
 #include <cstdint>
@@ -90,14 +90,14 @@ extern "C"
     typedef std::reverse_iterator<const function_type*> const_reverse_iterator;
   };
 
-  extern ctor_type::function_type _ctors_end[];
-  extern ctor_type::function_type _ctors_begin[];
+  extern ctor_type::function_type _ctors_end;
+  extern ctor_type::function_type _ctors_begin;
 }
 
 void crt::init_ctors()
 {
-  std::for_each(ctor_type::const_reverse_iterator(_ctors_end),
-                ctor_type::const_reverse_iterator(_ctors_begin),
+  std::for_each(ctor_type::const_reverse_iterator(&_ctors_end),
+                ctor_type::const_reverse_iterator(&_ctors_begin),
                 [](const ctor_type::function_type pf)
                 {
                   pf();
