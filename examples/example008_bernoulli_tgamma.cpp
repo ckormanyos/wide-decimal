0ce240ec464e70833b14c7325275d9f64301b084
ckormanyos <ckormanyos@yahoo.com>
2024-11-29 09:15:18 +0100

    Handle compilation on bare-metal ARM

--- examples/example008_bernoulli_tgamma.cpp
+++ examples/example008_bernoulli_tgamma.cpp
@@ -29,7 +29,9 @@
 
 #include <examples/example_decwide_t.h>
 #include <math/wide_decimal/decwide_t.h>
+#if !defined(WIDE_DECIMAL_DISABLE_IOSTREAM)
 #include <test/stopwatch.h>
+#endif
 #include <util/memory/util_n_slot_array_allocator.h>
 #include <util/utility/util_dynamic_array.h>
 
